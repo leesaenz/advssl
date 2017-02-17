f0d5aa53aa490b7e00fc58e939244a58fd8ca6bf
Lee Saenz <leesaenz@gmail.com>
2017-02-16 20:52:45 -0700

    Adds more CRUD functionality.

--- server/routes/api.js
+++ server/routes/api.js
@@ -17,20 +17,21 @@ router.use(function(req, res, next) {
 });
 
 router.get('/', function(req, res) {
-  res.json({ message: `Movie API: Access Granted!` });
+  res.json({ message: `Movie API be here.` });
 });
 
 router.route('/movies')
 
+//GET and list
 .get(function(req,res) {
   Movie.find(function(err, movies) {
     if(err)
       res.send(err);
     res.json(movies);
-    console.log(req.body);
   });
 })
 
+//POST / ADD
 .post(function(req,res) {
   var movie = new Movie(req.body);
 
@@ -42,6 +43,54 @@ router.route('/movies')
   });
 
 });
+
+router.route('/movies/:movie_id')
+
+//GET and list
+.get(function(req,res) {
+  Movie.findById(req.params.movie_id, function(err, movie) {
+    if(err)
+      res.send(err);
+    res.json(movie);
+  });
+})
+
+//PUT / UPDATE
+.put(function(req,res) {
+  Movie.findById(req.params.movie_id, function(err, movie) {
+    if (err)
+      res.send(err);
+
+    movie.title = req.body.title;
+    movie.director = req.body.director;
+    movie.year = req.body.year;
+    movie.cover = req.body.cover;
+    movie.description = req.body.description;
+    movie.genre = req.body.genre;
+    movie.imdb = req.body.imdb;
+    movie.tomatoes = req.body.tomatoes;
+
+    movie.save(function(err) {
+      if(err)
+        res.send(err);
+      res.json({ message: 'Movie Updated!'});
+    });
+  });
+})
+
+//DELETE
+.delete(function(req, res) {
+  Movie.remove({
+      _id: req.params.movie_id
+  }, function(err, bear) {
+      if (err)
+          res.send(err);
+
+      res.json({ message: 'Movie Deleted' });
+  });
+});
+
+
 
 //Redirect any other routes to index.  The route denoted with * must come AFTER all other API routes have been defined.
 router.get('*', (req, res) => {
